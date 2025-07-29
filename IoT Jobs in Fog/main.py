eb78b734b19be8ba60c72b5cbeacc225a6830db9
Mohammad Kadkhodaei <mohammadkad@gmail.com>
2025-07-29 11:54:59 +0330

    Update main.py
    Add main function.

--- IoT Jobs in Fog/main.py
+++ IoT Jobs in Fog/main.py
@@ -2,3 +2,11 @@
 # by : Mohammad Kadkhodaei
 # for: Deep reinforcement learning-based optimal deployment of IoT machine learning jobs in fog computing architecture
 # doi: https://doi.org/10.1007/s00607-024-01353-3
+
+
+def main():
+  # 1404-05-07
+  print("\nFinal Results:")
+  
+if __name__ == "__main__":
+    main()
